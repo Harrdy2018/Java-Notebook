b8469d936939863a6250afbc0cfc1dd49dfd3fe1
Harrdy <1035525823@qq.com>
2019-05-25 11:28:16 +0800

    Update mste.java

--- mste.java
+++ mste.java
@@ -5,6 +5,7 @@ package com.lukang.www;
  * 常量与常量的拼接结果在常量池。且常量池中不会存在相同内容的常量。
  * 只要其中有一个是变量，结果就在堆中
  * 如果拼接的结果调用intern()方法，返回值就在常量池中
+ * 如果String被final修饰，则是常量，常量+常量还是常量
  */
 public class KK {
 
@@ -15,9 +16,12 @@ public class KK {
     String s4=s1+"world";
     String s5=s1+s2;
     String s6=(s1+s2).intern();
+    final String s7="hello";
+    String s8=s7+"world";
     System.out.println(s3==s4);//false
     System.out.println(s3==s5);//false
     System.out.println(s4==s5);//false
     System.out.println(s3==s6);//true
+    System.out.println(s3==s8);//true
   }
 }
